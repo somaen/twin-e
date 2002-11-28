2cd8ef377436dc07342ad9610bfeccd9e4eb33ac
yazoo <yazoo@b6842851-fe11-0410-aa3d-efb4359767e1>
2002-11-28 22:23:57 +0000

    add threadcode.cpp
    git-svn-id: https://twin-e.svn.sourceforge.net/svnroot/twin-e/trunk@8 b6842851-fe11-0410-aa3d-efb4359767e1

---
+++ twin-e/src/threadCode.c
@@ -0,0 +1,22 @@
+#include "lba.h"
+#include "SDL.h"
+#include "SDL_thread.h"
+
+LBA_engine* tempEngine;
+
+int threadTimer(void* test)
+{
+	while(1)
+	{
+		SDL_Delay(50);
+		tempEngine->musicPosition++;
+		tempEngine->time++;
+	}
+	return(0);
+}
+
+void startThreadTimer(LBA_engine* lengine)
+{
+	tempEngine=lengine;
+	SDL_CreateThread(threadTimer,NULL);
+}
